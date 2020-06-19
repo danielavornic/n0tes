bb8a0e974370e60a8e4e266a5f9a6e0874f3b1ea
Daniela <daniela.vornic@gmail.com>
2020-06-19 17:03:55 +0300

    add the signup functionalities

--- n0tes/urls.py
+++ n0tes/urls.py
@@ -4,6 +4,6 @@ from notes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('signup/', views.signupuser, name='signupuser'),
+    path('notes/', views.notes, name='notes'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signupuser(request):
-    return render(request, 'notes/signupuser.html', {'form': UserCreationForm()})
+    if request.method == "GET":
+        return render(request, 'notes/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('notes')
+            except IntegrityError:
+                return render(request, 'notes/signupuser.html', {'form': UserCreationForm(), 'error': 'That username has already been taken'})
+        else:
+            return render(request, 'notes/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+def notes(request):
+    return render(request, 'notes/notes.html')
