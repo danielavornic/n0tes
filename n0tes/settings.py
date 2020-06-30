1de357c3c40eeafa5517ce011e00903f2fc37317
Daniela <daniela.vornic@gmail.com>
2020-06-30 18:24:14 +0300

    add heroku to allowed hosts

--- n0tes/settings.py
+++ n0tes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ld3yt+k2!oi9fd)xt_fofok=f%2(+5f*8m5qad$pu48a)799hh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['n0tesapp.herokuapp.com']
 
 
 # Application definition
