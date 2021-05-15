763ac09c41498aa34d0592b3983107822a5cbe03
Daniela Vornic <daniela.vornic@gmail.com>
2021-05-16 01:37:48 +0300

    try again to hide fixed footer when virtual keyboard appears on mobile

--- notes/static/notes/notes.js
+++ notes/static/notes/notes.js
@@ -2,14 +2,11 @@ $('.note-text').each(function(){
     $(this).html($(this).html().replace(/&amp;nbsp;/gi,''));
 });
 
-var isTouch = ('ontouchstart' in document.documentElement);
 var originalHeight = $(window).height();
 $(window).resize(function() {
-    if (isTouch) {
-        if ($(window).height() < originalSize){
-            $('#menu').hide();
-        } else {
-            $('#menu').show();
-        }
+    if ($(window).height() < originalSize){
+        $('#menu').hide();
+    } else {
+        $('#menu').show();
     }
 });
