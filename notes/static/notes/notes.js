46073147fce5d9b1c53e1fa6fe84291300796e8d
Daniela Vornic <daniela.vornic@gmail.com>
2021-05-16 01:08:34 +0300

    test to let mobile keyboard over content w jquery

--- notes/static/notes/notes.js
+++ notes/static/notes/notes.js
@@ -2,20 +2,11 @@ $('.note-text').each(function(){
     $(this).html($(this).html().replace(/&amp;nbsp;/gi,''));
 });
 
-var menu = $('#menu');
-var nav = $('nav').first();
-var toggle = $('#toggle');
-
-toggle.click(function() {
-    if (menu.outerHeight() == 60) {
-        menu.css('height', '90vh');
-        nav.css('display', 'block');
-        toggle.children().first().css('transform', 'rotate(-45deg) translate(-4px, 4px)');
-        toggle.children().eq(1).css('opacity', '0');
-        toggle.children().eq(2).css('transform', 'rotate(45deg) translate(-8px, -8px)');
+var originalSize = $(window).width() + $(window).height();
+$(window).resize(function() {
+    if ($(window).width() + $(window).height() != originalSize){
+        $('#menu').hide();
     } else {
-        menu.css('height', '60px');
-        nav.css('display', 'none');
-        toggle.children().removeAttr("style");
+        $('#menu').show();
     }
-})
+});
