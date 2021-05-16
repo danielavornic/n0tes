17e9066f625d74907002e9a0f7ebc2677aa6ee4c
Daniela Vornic <daniela.vornic@gmail.com>
2021-05-16 23:40:29 +0300

    make controlls of note responsive for mobile

--- notes/static/notes/notes.js
+++ notes/static/notes/notes.js
@@ -12,15 +12,3 @@ var $grid = $('#notes ul').isotope({
 $('.note-text').each(function(){
     $(this).html($(this).html().replace(/&amp;nbsp;/gi,''));
 });
-
-const touch = matchMedia('(hover: none)').matches;
-const originalHeight = $(window).height();
-$(window).resize(function() {
-    if (touch) {
-        if ($(window).height() < originalHeight){
-            $('#menu').hide();
-        } else {
-            $('#menu').show();
-        }
-    }
-});
